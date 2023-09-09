f8f4af408c9fa460c8cbd912da658b36953268a2
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-09 19:44:57 +0900

    [Add] tests/top.html.erbに記述

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,7 +1,7 @@
 let title = "javascriptが使えました";
 alert(title);
 
-$(document).ready(function () { 
+$(document).ready(function () {  
   $('.jquery').on('click', function(){
     $(this).css('color','red');
   });
